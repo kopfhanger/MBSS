34e6e1f26569a9c812806a17d8ee7431bb4ae915
kopfhanger <xinyangzengyike@qq.com>
2025-02-14 21:56:55 +0800

    null

---
+++ cmake-build-debug/CMakeFiles/mbss.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mbss.dir/main.cpp.o"
+  "CMakeFiles/mbss.dir/main.cpp.o.d"
+  "mbss"
+  "mbss.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mbss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
